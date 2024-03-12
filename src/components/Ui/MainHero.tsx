27b036991dec8b1776b429264ebd59a7bae7e75b
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-03-12 10:13:38 +0100

    F

--- src/components/Ui/MainHero.tsx
+++ src/components/Ui/MainHero.tsx
@@ -6,7 +6,7 @@ export default function MainHero(props:any) {
     const router = useRouter()
     const path = router.pathname
     return (
-        <section className={path === "/" ? "bg-[url(/bgs/hero02.svg)] bg-cover bg-center w-full pt-[170px] pb-[480px]":"bg-[url(/bgs/hero.svg)] bg-cover bg-center w-full pt-[170px] pb-[480px]"}>
+        <section className={path === "/" ? "bg-[url(/bgs/hero.svg)] bg-cover bg-center w-full pt-[170px] pb-[480px]":"bg-[url(/bgs/hero.svg)] bg-cover bg-center w-full pt-[170px] pb-[480px]"}>
             <div className="text-white text-center w-[94%] mx-auto max-w-[1100px]">
                 <Reveal delayTime={0}>
                     <h1 className="font-[300] tracking-tight text-[70px] leading-[80px] mb-[14px]">{props.heading}</h1>
@@ -15,12 +15,12 @@ export default function MainHero(props:any) {
                     <p className="font-[200] text-[18px] w-[80%] mx-auto text-gray-200 max-w-[1000px]">{props.paragraph}</p>
                 </Reveal>
                 {path === "/" ? 
-                
-                <div className="flex items-center justify-center gap-[16px] mt-[50px]">
+                <Reveal delayTime={0.2}>
+                    <div className="flex items-center justify-center gap-[16px] mt-[50px]">
                      <Link className="bg-white text-main text-center px-[24px] py-[10px] text-[15px] rounded-full font-[300]" href={"/"}>Join Waitlist</Link>
                      <Link className="bg-white text-main text-center px-[24px] py-[10px] text-[15px] rounded-full font-[300]" href={"/pricing"}>View Pricing</Link>
-
-                </div>
+                    </div>
+                </Reveal>
                 :
                 null}
             </div>
--- src/components/comparison.tsx
+++ src/components/comparison.tsx
@@ -2,6 +2,6 @@ import SectionStarter from "./Ui/SectionStarter";
 
 export default function Comparison() {
     return (
-        <SectionStarter heading="Why choose ToolBird" paragraph="Wake up to an inbox full of new testimonial alerts. Share them everywhere in seconds. Get more visits, convert more, and close more deals. Senja makes it easy." />
+        <SectionStarter heading="All tools in one place" paragraph="Wake up to an inbox full of new testimonial alerts. Share them everywhere in seconds. Get more visits, convert more, and close more deals. Senja makes it easy." />
     )
 }
