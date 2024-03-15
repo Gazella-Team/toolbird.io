26f9b05791fb50dcd10b63e4ce1f1753aeb89c6b
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-03-15 12:41:39 +0100

    guides

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import Comparison from "@/components/comparison";
 export default function Home() {
   return (
     <Layout mode="">
-      <MainHero heading="All the tools your SaaS needs to start, scale and run" paragraph="Wake up to an inbox full of new testimonial alerts. Share them everywhere in seconds. Get more visits, convert more, and close more deals. Senja makes it easy." />
+      <MainHero heading="Introducing ToolBird Guides" paragraph="Wake up to an inbox full of new testimonial alerts. Share them everywhere in seconds. Get more visits, convert more, and close more deals. Senja makes it easy." />
       <section>
         <RevealLight delayTime={0.2}>
           <div className="w-[90%] mx-auto max-w-[1200px] mt-[-400px]">
