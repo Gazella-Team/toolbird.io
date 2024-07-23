8bebc7937eb1cce5825180a33b5fda36493e5e67
Simon Maribo <simonmaribo@outlook.dk>
2024-07-23 22:47:28 +0200

    Pricing

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -4,18 +4,19 @@ import PricingTable from '@/components/pricingtable'
 import PricingFAQ from '@/components/FAQ/PricingFAQ'
 import Meta from '@/components/Layout/Meta'
 import CurvePageIntro from '@/components/CurvePageIntro'
+import PricingSliderBox from '@/components/pricing/PricingSliderBox'
 
 export default function Pricing() {
 	return (
 		<Layout whiteBeforeScroll={true} white={true}>
 			<Meta title={`Pricing - Toolbird.io`} />
 			<CurvePageIntro
-				hook="Flexible pricing"
+				hook="A price for every project"
 				highlighted=""
-				heading="Pricing for everyone"
-				paragraph="We offer plans that suit any project. Get started in just 1 minute."
+				heading="Pricing"
+				paragraph="Choose the plan that fits your needs. All plans come with a 14-day free trial."
 			>
-				<PricingTable />
+				<PricingSliderBox />
 			</CurvePageIntro>
 			{
 				<BlurSection
